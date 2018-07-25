f778bc7bf028b0584286bd8f6df1f11fe03a3b71
me <famlinkus@gmail.com>
2018-07-25 14:02:19 +0530

    update

---
+++ flask_tools/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='flask_tools',
+    version='1.0',
+    description='Flask Tools,timesavers and content encoding',
+    author='Hydrophobefireman',
+    author_email='None',
+    url='https://github.com/hydrophobefireman/flask_tools/',
+    packages=['distutils', 'distutils.command'],
+)
