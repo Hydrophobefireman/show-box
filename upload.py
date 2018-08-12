6fdb092e26a0eea8de862e45a2598e44b82c8faf
me <bhavesh.pareek36@gmail.com>
2018-08-12 13:38:15 +0530

    update

--- upload.py
+++ upload.py
@@ -5,18 +5,16 @@ import os
 def upload(imgurl):
     clapi_key = os.environ.get("key")
     clapi_secret = os.environ.get("cl_secret")
-    if clapi_key is None: 
+    if clapi_key is None:
         try:
             with open("a.cloudinary", "r") as f:
-                clapi_key = f.read()
+                clapi_key = f.read().strip()
             with open("b.cloudinary", "r") as f:
-                clapi_secret = f.read()
+                clapi_secret = f.read().strip()
         except:
             raise Exception("no key provided")
     cloudinary.config(
-        cloud_name="media-proxy",
-        api_key=clapi_key,
-        api_secret=clapi_secret
+        cloud_name="media-proxy", api_key=clapi_key, api_secret=clapi_secret
     )
     a = cloudinary.uploader.upload(imgurl)
     return a
@@ -25,4 +23,4 @@ def upload(imgurl):
 if __name__ == "__main__":
     data = upload(input("enter url:"))
     print("Raw Data:%s\n\n" % (data))
-    print(data.get('secure_url'))
+    print(data.get("secure_url"))
